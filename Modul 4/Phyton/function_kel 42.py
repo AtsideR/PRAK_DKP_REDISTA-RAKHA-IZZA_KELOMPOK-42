f5cbac299ef46acc5d1c0718ccc975e6c557e2f1
AtsideR <redistaizza@gmail.com>
2024-03-24 03:26:11 +0700

    praktikum dkp

--- Modul 4/Phyton/function_kel 42.py
+++ Modul 4/Phyton/function_kel 42.py
@@ -1,8 +1,8 @@
 from percobaan2 import*
- #Functiondengannonreturntype
+
 def non_return_func(praktikan1, praktikan2):
     print(f"Selamat Datang di Praktikum DKP 2022 {praktikan1} dan {praktikan2}")
- #Functiondenganreturntype
+
 def return_func(shift):
     print(f"Shift kalian adalah",shift)
     if(shift==1)or(shift==2):
@@ -10,14 +10,14 @@ def return_func(shift):
     else:
         return print("Tidak ada shift tersebut")
     
- #FunctiondenganArbitraryType
+
 def arbitrary_func(*penutup):
         for nama in penutup:
             print("Terimakasih",nama)
             
- #AnonymousFunction
+
 anonim_func = lambda praktikan1, praktikan2, kelompok : print(f"Ini adalah percobaan {praktikan1} dan {praktikan2}kelompok ",kelompok)
- #PemanggilanFungsi
+
 non_return_func("nama1","nama2")
 return_func(7)
 anonim_func("nama1","nama2",42)
--- Modul 4/Phyton/percobaan2.py
+++ Modul 4/Phyton/percobaan2.py
@@ -1,12 +1,12 @@
 class contoh_method:
- #initmethod
+
     def __init__(self, praktikan1, praktikan2):
         self.praktikan1=praktikan1
         self.praktikan2=praktikan2
- #selfparameter
+
     def mulai(self):
          print (f"Selamat Datang di Percobaan 2 {self.praktikan1} dan {self.praktikan2}")
- #methoddenganparameter 
+
     def selesai(self, waktu):
         print("Percobaan akan selesai dalam : ")
         while waktu >0:
